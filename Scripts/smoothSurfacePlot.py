e6b99ef56b6ebf60c04c6346c0da31cbeb84cc30
sethzima <70332521+sethzima@users.noreply.github.com>
2023-08-23 16:12:36 -0500

    Add files via upload

--- Scripts/smoothSurfacePlot.py
+++ Scripts/smoothSurfacePlot.py
@@ -7,13 +7,18 @@ import seaborn as sns
 from scipy.ndimage import uniform_filter
 
 # Load the data from the CSV file
-data = pd.read_csv('C:/Users/HP/Documents/Python/Research/Lactate/6_3_Lactate_1_WELL_arranged_csv.csv')
+data = pd.read_excel('R:/Lab Share/Seth/Luminescence Tech/Spatial luminescence/4 metabolites/Processed Data/6.27.23-6.30.23 4 metabolites/6.30.23 4 metabolites 72hr/Lactate 72 hr/6.30.23-4metab-Lactate-72hr-Rep1-9pts.xlsx'
+                     , sheet_name='Matrix0', header=None)
 # rows_to_read = list(range(20,35))
 # selected_rows = data.iloc[rows_to_read]
 
 #print(data)
 
-#sns.heatmap(data, cmap='viridis')
+plt.figure()
+sns.heatmap(data, cmap='inferno')
+plt.title("Four metabolites: Lactate 72hr Raw")
+plt.savefig('4metab_lactate_72hr_heat_raw1.jpg', format='jpeg')
+plt.close()
 
 # Extract the x, y, and z values from the data
 y = list(range(1, len(data) + 1))
@@ -30,9 +35,9 @@ x_grid, y_grid = np.meshgrid(x, y)
 #z_grid = z.reshape(x_grid.shape)
 
 # Apply moving average smoothing
-z1 = uniform_filter(z, size=5)  # Adjust the size parameter for different levels of smoothing
+z1 = uniform_filter(z, size=3)  # Adjust the size parameter for different levels of smoothing
 
-z2 = uniform_filter(z1, size=5)
+z2 = uniform_filter(z1, size=3)
 
 # Create a 3D plot
 fig = plt.figure()
@@ -40,13 +45,19 @@ ax = fig.add_subplot(111, projection='3d')
 
 # Plot the surface
 ax.plot_surface(x_grid, y_grid, z2, cmap = 'inferno', antialiased=True)
-
+ax.grid(False)
 
 # Set labels and title
+ax.view_init(elev=35, azim=45)
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
-plt.title('Lactate 1hr')
 
+plt.title('Four metabolites: Lactate 72hr Raw')
+plt.savefig('4metab_lactate_72hr_3d_raw1.jpg', format='jpeg')
 # Display the plot
+
 plt.show()
+
+
+
